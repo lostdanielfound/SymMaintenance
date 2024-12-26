5a445b8d27eb54a2a1cb2331e243cb01933872f4
lostdanielfound <daniellostsilver2000@gmail.com>
2024-12-26 01:28:31 -0800

    Started implementation of Screen

--- presentation/src/main/java/com/project/presentation/features/HomeScreen.kt
+++ presentation/src/main/java/com/project/presentation/features/HomeScreen.kt
@@ -1,6 +1,30 @@
 package com.project.presentation.features
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen() {
-
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = { Text("Home Screen") }
+            )
+        },
+    ) { paddingValues ->
+        Surface(
+            modifier = Modifier
+                .padding(paddingValues)
+        ) {
+            DropdownMenu() { }
+        }
+    }
 }
