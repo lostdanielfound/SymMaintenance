32a8dab9fa4575fa70ed1ff0504fb310b27c5a3a
lostdanielfound <daniellostsilver2000@gmail.com>
2024-12-26 23:41:06 -0800

    Finish with design, need to implement interaction

--- presentation/src/main/java/com/project/presentation/features/DropDownList.kt
+++ presentation/src/main/java/com/project/presentation/features/DropDownList.kt
@@ -3,6 +3,8 @@ package com.project.presentation.features
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -15,11 +17,13 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.project.presentation.data.generateFakeData
+import com.project.presentation.data.GenerateFakeData
 import com.project.presentation.models.Task
 import java.util.UUID
 
@@ -27,7 +31,8 @@ import java.util.UUID
 fun DropDownList(
     expanded: Boolean,
     title: String,
-    icon: @Composable() () -> Unit,
+    icon: ImageVector,
+    dropDownDescription: String?,
     onPressedRequest: () -> Unit,
     onDismissRequest: () -> Unit,
     modifier: Modifier = Modifier,
@@ -35,16 +40,21 @@ fun DropDownList(
     content: @Composable() (ColumnScope.() -> Unit)
 ) {
     Surface(
-        shape = RoundedCornerShape(1.dp),
-        border = BorderStroke(1.dp, Color.Black)
+        shape = RoundedCornerShape(6.dp),
+        border = BorderStroke(1.dp, Color.Gray),
+        modifier = Modifier
+            .padding(4.dp)
     ) {
         Row(
+            verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier
+                .fillMaxWidth()
                 .padding(8.dp)
         ) {
-            icon()
+            Icon(icon, contentDescription = dropDownDescription)
+            Spacer(modifier = Modifier.padding(horizontal = 8.dp))
             Text(text = title)
-
+            Spacer(modifier = Modifier.fillMaxWidth(0.9f))
             if (expanded) {
                 Icon(
                     Icons.Filled.KeyboardArrowDown,
@@ -76,13 +86,15 @@ fun DropDownList(
 fun PreviewDropDownList() {
     val expanded = false
     val title = "Current Tasks"
+    val dropDownDescription = "Current Outstanding tasks"
 
-    val exampleList = generateFakeData.generateFakeTaskList()
+    val exampleList = GenerateFakeData.generateFakeTaskList()
 
     DropDownList(
         expanded = expanded,
         title = title,
-        icon = { Icon(Icons.Filled.Build, contentDescription = null) },
+        icon = Icons.Filled.Build,
+        dropDownDescription = dropDownDescription,
         onPressedRequest = {},
         onDismissRequest = {},
         items = exampleList
