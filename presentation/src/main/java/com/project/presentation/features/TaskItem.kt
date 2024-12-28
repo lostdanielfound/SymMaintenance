1fa4b3b0d28bcdb6f8fc53ce680bf13b2bc3ab8f
lostdanielfound <daniellostsilver2000@gmail.com>
2024-12-27 23:22:53 -0800

    started on implementation

--- presentation/src/main/java/com/project/presentation/features/TaskItem.kt
+++ presentation/src/main/java/com/project/presentation/features/TaskItem.kt
@@ -1,9 +1,19 @@
 package com.project.presentation.features
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.project.presentation.data.GenerateFakeData
 import com.project.presentation.models.Task
 
 /**
@@ -16,9 +26,25 @@ import com.project.presentation.models.Task
 @Composable
 fun TaskItem(task: Task) {
     Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
         modifier = Modifier
-            .fillMaxWidth()
+            .height(IntrinsicSize.Min)
     ) {
+        Row(
 
+        ) {
+            Text("M " + task.module.toString())
+            VerticalDivider(color = MaterialTheme.colorScheme.secondary, modifier = Modifier.padding(horizontal = 4.dp))
+            Text("Lv. " + task.level.toString())
+            VerticalDivider(color = MaterialTheme.colorScheme.secondary, modifier = Modifier.padding(horizontal = 4.dp))
+            Text("Ave. " + task.avenue.toString())
+        }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TaskItemPreview() {
+    val fakeTask = GenerateFakeData.generateFakeTaskList()[0]
+    TaskItem(fakeTask)
 }
