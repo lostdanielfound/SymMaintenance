8617f61bb89c2bc081bbfd58ce15e1e8c61a960f
lostdanielfound <daniellostsilver2000@gmail.com>
2025-01-07 03:40:22 -0800

    Project config

---
+++ app/src/main/java/com/project/symmaintenance/ui/routes/HomeScreen.kt
@@ -0,0 +1,8 @@
+package com.project.symmaintenance.ui.routes
+
+object Routes {
+    const val Home = "home"
+    const val Task = "task/{taskId}"
+    const val Init = "init"
+    const val Settings = "settings"
+}
