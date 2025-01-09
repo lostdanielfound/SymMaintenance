545ee10161f7f7faba6bacc3e5c1701caac2f4c5
lostdanielfound <daniellostsilver2000@gmail.com>
2025-01-09 03:40:27 -0800

    Reorganized structure and add Testing TextButton

--- presentation/src/main/java/com/project/presentation/features/HomeScreen/HomeScreen.kt
+++ presentation/src/main/java/com/project/presentation/features/HomeScreen/HomeScreen.kt
@@ -8,14 +8,26 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.project.presentation.data.GenerateFakeData
 import com.project.presentation.features.DropDownList
+import com.project.symmaintenance.ui.routes.Routes
 
+/**
+ * HomeScreen
+ *
+ * @param onTaskRequest Callback function that's passed to TaskItems.
+ * TODO: (Possible have the ViewModel implement this function instead)
+ * FOR TESTING: all TaskItems when clicked on will navigate to task screen
+ */
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomeScreen() {
+fun HomeScreen(
+    onTaskRequest: (String) -> Unit,
+    onClickRequestTesting: (String) -> Unit
+) {
     /* During recomposition, the homescreen is suppose to fill in the list of tasks respectively after
     * pulling the list of all tasks from server.
     *
@@ -49,6 +61,9 @@ fun HomeScreen() {
                 icon = Icons.Filled.Build,
                 dropDownDescription = "All Shift Tasks",
                 onPressedRequest = {},
+                {},
+                {},
+                {},
                 items = allTasks
             )
             DropDownList(
@@ -56,6 +71,9 @@ fun HomeScreen() {
                 icon = Icons.Filled.Build,
                 dropDownDescription = "Current Shift Tasks",
                 onPressedRequest = {},
+                {},
+                {},
+                {},
                 items = currentTasks
             )
             DropDownList(
@@ -63,9 +81,18 @@ fun HomeScreen() {
                 icon = Icons.Filled.Build,
                 dropDownDescription = "Completed Shift Tasks",
                 onPressedRequest = {},
+                {},
+                {},
+                {},
                 items = completeTasks
             )
             // TODO: Will need to make a filter DropDownList later on
+
+            // TODO: Navigation Testing
+            TextButton(
+                onClick = { onClickRequestTesting(Routes.Init) }
+            ) { Text("Navigate to InitScreen")}
+
         }
     }
 }
