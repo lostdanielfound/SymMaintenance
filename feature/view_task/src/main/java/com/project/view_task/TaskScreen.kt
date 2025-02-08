89165d88b0c12832d7145a28535a2d91402c5125
lostdanielfound <daniellostsilver2000@gmail.com>
2025-02-07 23:43:50 -0800

    Started adding task details

--- feature/view_task/src/main/java/com/project/view_task/TaskScreen.kt
+++ feature/view_task/src/main/java/com/project/view_task/TaskScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
 import androidx.compose.material.icons.filled.KeyboardArrowLeft
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
@@ -14,6 +15,9 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import com.project.framework.Task
 
 /**
@@ -42,8 +46,23 @@ fun TaskScreen(
     ) { paddingValues ->
         Surface(modifier = Modifier.padding(paddingValues)) {
             Column(
-
             ) {
+                if (task.botId != null) {
+                    Text("Bot ${task.botId}")
+                    Text("ECODE: ${task.fatalError}")
+                }
+
+                Text(
+                    text = "Location",
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+                )
+                HorizontalDivider()
+                task.module?.let { Text("Mod ${task.module}") }
+                task.level?.let { Text("Level ${task.level}") }
+                task.avenue?.let { Text("Avenue ${task.avenue}") }
+                task.aisle?.let { Text("Aisle ${task.aisle}") }
+                task.mvc?.let { Text("MVC ${task.mvc} side ${task.mvcSide}") }
 
             }
         }
