5a3b619d5ba5e8c7c159b930b7e0b39881cca070
lostdanielfound <daniellostsilver2000@gmail.com>
2025-02-07 21:34:34 -0800

    Started on Layout of screen and topbar

--- feature/view_task/src/main/java/com/project/view_task/TaskScreen.kt
+++ feature/view_task/src/main/java/com/project/view_task/TaskScreen.kt
@@ -1,6 +1,20 @@
 package com.project.view_task
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.project.framework.Task
 
 /**
  * TaskScreen
@@ -8,7 +22,30 @@ import androidx.compose.runtime.Composable
  * @param The selected taskId from the homeScreen, in case if a null value is given,
  * screen will show an erroneous page explaining to user.
  */
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TaskScreen(taskId: String?) {
+fun TaskScreen(
+    task: Task,
+    onClickBack: () -> Unit,
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Task Details") },
+                navigationIcon = {
+                    IconButton(onClick = { onClickBack() } ) {
+                        Icon(Icons.AutoMirrored.Filled.KeyboardArrowLeft, contentDescription = "Back to Home Screen")
+                    }
+                }
+            )
+        }
+    ) { paddingValues ->
+        Surface(modifier = Modifier.padding(paddingValues)) {
+            Column(
 
+            ) {
+
+            }
+        }
+    }
 }
